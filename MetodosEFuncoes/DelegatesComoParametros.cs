74a696bba7928d7e0801c8545b85c0d9b60395c3
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2023-11-08 17:03:15 -0300

    Delegate como parâmetro

---
+++ MetodosEFuncoes/DelegatesComoParametros.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.MetodosEFuncoes {
+    internal class DelegatesComoParametros {
+        public delegate int Operacao(int x, int y);
+
+        public static int Soma(int x, int y) {
+            return x + y;
+        }
+
+        public static string Calculadora(Operacao operacao, int x, int y) {
+            var resultado = operacao(x, y);
+
+            return "Resultado: " + resultado;
+        }
+
+        public static void Executar() {
+            Operacao subtracao = (int x, int y) => x - y;
+
+            Console.WriteLine(Calculadora(subtracao, 3, 2));
+            Console.WriteLine(Calculadora(Soma, 3, 2));
+        }
+    }
+}
