9b1827f72f77ad1ced4ad4a57f67eb5b6ba18de9
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2023-11-08 16:10:47 -0300

    Lambda como delegate

---
+++ MetodosEFuncoes/LambdasDelegate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.MetodosEFuncoes {
+    delegate double Operacao(double x, double y);
+
+    internal class LambdasDelegate {
+        public static void Executar() {
+            Operacao soma = (x, y) => x + y;
+            Operacao subtracao = (x, y) => x - y;
+            Operacao multiplicacao = (x, y) => x * y;
+
+            Console.WriteLine(soma(3, 3));
+            Console.WriteLine(subtracao(5, 2));
+            Console.WriteLine(multiplicacao(12, 8));
+        }
+    }
+}
