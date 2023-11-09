8cbb531f0a91dd58e0ffda30046a4758796d2cc7
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2023-11-09 17:25:20 -0300

    Exceções

---
+++ Excecoes/PrimeiraExcecao.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Excecoes {
+    public class Conta {
+        double Saldo;
+
+        public Conta(double saldo) {
+            Saldo = saldo;
+        }
+
+        public void Sacar(double valor) {
+            if (valor > Saldo) {
+                throw new ArgumentException("Saldo insuficiente.");
+            }
+
+            Saldo -= valor;
+        }
+    }
+
+    internal class PrimeiraExcecao {
+        public static void Executar() {
+            var conta = new Conta(1_223.45);
+
+            try {
+                conta.Sacar(1600);
+                Console.WriteLine("Retirada com sucesso!");
+            } catch (Exception ex) { // ArgumentException ex
+                Console.WriteLine(ex.Message);
+            } finally {
+                Console.WriteLine("Obrigado!");
+            }
+        }
+    }
+}
